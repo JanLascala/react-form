75997ef9caf9dfe83bf9ff5f6ca44ab2bfbb69a6
JanLascala <lascala.jan@gmail.com>
2025-03-26 14:48:19 +0100

    initialize React project with Vite setup and basic components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//other imports
+
+function App() {
+  return (
+    <>
+      <header />
+
+    </>)
+
+}
+
+export default App
