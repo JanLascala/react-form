0fd50198e258245a88ceb07b103c62506ad7bd9d
JanLascala <lascala.jan@gmail.com>
2025-03-26 15:38:47 +0100

    corrected form add title and style

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,14 @@
 export default function Form() {
     return (
         <div className="form text-center">
-            <div class="mb-3">
+            <div className="mb-3">
                 <label for="essay" className="form-label text-center fs-3 text fw-bold "> Write & click when you're finished</label>
-                <textarea class="form-control" id="essay" rows="3"></textarea>
 
-                <button type="button" class="btn btn-light mt-3">Save</button>
+                <textarea className="form-control Title_form" id="Title" rows="3" placeholder="Your title"></textarea>
+
+                <textarea className="form-control" id="essay" rows="3" placeholder="Your post"></textarea>
+
+                <button type="button" className="btn btn-light mt-3">Save</button>
             </div>
         </div>
     )
